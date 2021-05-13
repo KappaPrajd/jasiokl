995db0a25be8b140df5f97a23f7ade79c47091a2
KappaPrajd <tomaszmolski22@wp.pl>
2021-05-13 14:49:45 +0200

    created header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,28 +4,36 @@ import "./css/Header.css";
 const Header = () => {
   const [isActive, setIsActive] = useState(false);
 
+  const toggleHide = () =>
+    document.querySelector(".main").classList.toggle("hide");
+
   return (
-    <div className={`nav ${isActive ? "nav-active" : ""}`}>
-      <div className="nav-left">
-        <div
-          className={`hamburger ${isActive ? "hamburger-active" : ""}`}
-          onClick={() => setIsActive(!isActive)}
-        >
-          <div className="line line1"></div>
-          <div className="line line2"></div>
-          <div className="line line3"></div>
+    <React.Fragment>
+      <div className="nav">
+        <div className="nav-left">
+          <div
+            className={`hamburger ${isActive ? "hamburger-active" : ""}`}
+            onClick={() => {
+              setIsActive(!isActive);
+              toggleHide();
+            }}
+          >
+            <div className="line line1"></div>
+            <div className="line line2"></div>
+            <div className="line line3"></div>
+          </div>
         </div>
-      </div>
-      <div className="nav-right">
-        <h3>FB</h3>
-        <h3>IG</h3>
+        <div className="nav-right">
+          <h3>FB</h3>
+          <h3>IG</h3>
+        </div>
       </div>
       <div className={`nav-items ${isActive ? "nav-items-active" : ""}`}>
         <h2 className="nav-item">O mnie</h2>
         <h2 className="nav-item">Projekty</h2>
         <h2 className="nav-item">Kontakt</h2>
       </div>
-    </div>
+    </React.Fragment>
   );
 };
 
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,26 @@
 import React from "react";
 import Header from "./Header";
+import "./css/Home.css";
 
 const Home = () => {
   return (
     <React.Fragment>
       <Header />
+      <div className="main">
+        <div className="home-content">
+          <div className="left-panel"></div>
+          <div className="right-panel">
+            <img
+              className="portrait"
+              src="img/portrait1.png"
+              alt="portrait"
+            ></img>
+          </div>
+        </div>
+        <div className="footer">
+          <h3>Jan Kluczkiewicz</h3>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
