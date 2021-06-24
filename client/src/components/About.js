1024a12a7fe9fda9921e2395567d14464044f3a1
KappaPrajd <tomaszmolski22@wp.pl>
2021-06-24 23:02:18 +0200

    created About component

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,11 +1,30 @@
 import React from "react";
 import Header from "./Header";
+import Footer from "./Footer";
+import "./css/About.css";
 
 const About = () => {
   return (
     <React.Fragment>
       <Header />
-      <div className="main"></div>
+      <div className="main about">
+        <div className="about-images">
+          <div className="about-image"></div>
+          <div className="about-image"></div>
+        </div>
+        <div className="about-info">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem nisi fuga temporibus repellendus obcaecati tempore ab
+            reiciendis modi possimus tenetur, nulla molestiae sint nam quibusdam
+            quae explicabo dolorem facilis accusamus minima. Non culpa, nulla
+            quaerat unde harum doloremque libero nostrum accusamus, vitae sit
+            officiis voluptatem animi qui, corrupti cupiditate veritatis? Non
+            culpa
+          </p>
+        </div>
+      </div>
+      <Footer />
     </React.Fragment>
   );
 };
