ba7481bfe08c0c7a2d7da5f5a03f7528331b34a8
KappaPrajd <tomaszmolski22@wp.pl>
2021-07-07 20:46:09 +0200

    prepared for production

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.static("public"));
 
 const projects = require("./api/projects");
 app.use("/api", projects);
+
+//serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Jasiokl app listening at http://localhost:${port}`);
